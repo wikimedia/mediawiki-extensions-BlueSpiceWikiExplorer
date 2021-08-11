eb53a053316995e73e3932b44b9adc643f6a1f45
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-11 13:39:00 +0200

    Add extension to SkinSlot GlobalActionsTool
    [4.x]
    
    Change-Id: Ibe21ec7add51bfc31d826789dfb412cdc8cbaa2b

---
+++ src/HookHandler/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BlueSpice\WikiExplorer\HookHandler;
+
+use BlueSpice\WikiExplorer\GlobalActionsTool;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+
+class Main implements MWStakeCommonUIRegisterSkinSlotComponents {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		$registry->register(
+			'GlobalActionsTools',
+			[
+				'special-bluespice-wikiexplorer' => [
+					'factory' => static function () {
+						return new GlobalActionsTool();
+					}
+				]
+			]
+		);
+	}
+}
